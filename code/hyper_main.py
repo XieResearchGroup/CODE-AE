c2d820ec19aff302d37080713721091439fce753
Hidden He <he15810251026@gmail.com>
2020-12-01 11:49:48 -0800

    Update hyper_main.py

--- code/hyper_main.py
+++ code/hyper_main.py
@@ -207,7 +207,7 @@ if __name__ == '__main__':
 
     params_grid = {
         "pretrain_num_epochs": [10, 30, 50, 100, 150, 200, 250, 300],
-        "train_num_epochs": [100, 200, 300, 500, 1000, 1500, 2000]
+        "train_num_epochs": [100, 200, 300, 500, 750, 1000, 1500, 2000]
     }
 
     if args.method not in ['adsn', 'adae', 'dsnw']:
